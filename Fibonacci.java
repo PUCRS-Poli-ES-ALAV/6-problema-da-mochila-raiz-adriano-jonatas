218024e5bf3465d0156d08f2e95988da16deb9d1
Adriano Ramos <adriano.ramos@acad.pucrs.br>
2022-03-21 20:24:28 -0300

    memoized fibo and look up methods implemented

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,7 +1,7 @@
 public class Fibonacci {
 
 
-    // MARK: - Fibonacci recursivo
+    // MARK: - recursive fibonacci
     public int fibonacci(int n) {
         if (n<= 1) {
             return n;
@@ -9,5 +9,29 @@ public class Fibonacci {
 
         return fibonacci(n-1) + fibonacci(n-2);
     }
-    
+
+    // MARK: - Memoized fibonacci
+    public int memoizedFibo() {
+
+        return 0;
+    }
+
+    private int lookUpFibo(int[] f, int n) {
+        if (f[n] >= 0) {
+            return f[n];
+        }
+        
+        if (n<=1) {
+            f[n] = n;
+        } else {
+            f[n] = lookUpFibo(f, n - 1) + lookUpFibo(f, n - 2);
+        }
+
+        return f[n];
+    }
+//     MEMOIZED-FIBO (f, n)
+//  para i ← 0 até n faça
+//       f [i] ← −1
+//  devolva LOOKUP-FIBO (f, n)
+
 }
